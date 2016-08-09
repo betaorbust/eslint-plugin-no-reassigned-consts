558c60bcec81f7a177c4a01eeefb4df8e189c88f
Jacques Favreau <jfavreau@netflix.com>
2016-08-09 16:07:52 -0700

    Update npm modules.

--- tests/no-reassigned-consts.js
+++ tests/no-reassigned-consts.js
@@ -1,85 +1,63 @@
 'use strict';
 
-var eslint = require('eslint');
-var ESLintTester = require('eslint-tester');
+var ESLintTester = require('eslint').RuleTester;
+var rule = require('../rules/no-reassigned-consts');
 
-var eslintTester = new ESLintTester(eslint.linter);
+var eslintTester = new ESLintTester();
 
-eslintTester.addRuleTest('rules/no-reassigned-consts', {
+var parserOptions = {
+    ecmaVersion: 6
+};
+
+eslintTester.run('no-reassigned-consts', rule, {
     valid: [
-        {
-            code: 'var a = "a"; a = "b";'
-        },
+        'var a = "a"; a = "b";',
+        'var CONST_VAL = "Amy";',        
         {
             code: 'let a = "a"; a = "b";',
-            ecmaFeatures: {
-                blockBindings: true
-            }
+            parserOptions: parserOptions
         },
         {
             code: 'const a = "a";',
-            ecmaFeatures: {
-                blockBindings: true
-            }
+            parserOptions: parserOptions
         },
         {
             code: 'const a = {};',
-            ecmaFeatures: {
-                blockBindings: true
-            }
+            parserOptions: parserOptions
         },
         {
             code: 'const a = 0;',
-            ecmaFeatures: {
-                blockBindings: true
-            }
+            parserOptions: parserOptions
         },
         {
             code: 'const {a, b} = [1, 2];',
-            ecmaFeatures: {
-                blockBindings: true,
-                destructuring: true
-            }
+            parserOptions: parserOptions
         },
         {
             code: 'const a = {}; a.a = "a"; delete a.a; a.r = 0; ++a.r;',
-            ecmaFeatures: {
-                blockBindings: true
-            }
-        },
-        {
-            code: 'var CONST_VAL = "Amy";'
+            parserOptions: parserOptions
         }
     ],
     invalid: [
         {
             code: 'const {a, b} = [1, 2]; a = 3;',
             errors: [{ message: 'a is a constant and should not be reassigned.' }],
-            ecmaFeatures: {
-                blockBindings: true,
-                destructuring: true
-            }
+            parserOptions: parserOptions
         },
         {
             code: 'const b = "b"; b = "c";',
             errors: [{ message: 'b is a constant and should not be reassigned.' }],
-            ecmaFeatures: {
-                blockBindings: true
-            }
+            parserOptions: parserOptions
         },
         {
             code: 'const c = 0; ++c;',
             errors: [{ message: 'c is a constant and should not be reassigned.' }],
-            ecmaFeatures: {
-                blockBindings: true
-            }
+            parserOptions: parserOptions
         },
         {
             code: 'function test() { const d = 0; delete d; }',
             errors: [{ message: 'd is a constant and should not be reassigned.' }],
-            ecmaFeatures: {
-                blockBindings: true
-            }
+            parserOptions: parserOptions
         },
         {
             code: 'var CONST_VALUE = "Amy"; CONST_VALUE = "Rory";',
